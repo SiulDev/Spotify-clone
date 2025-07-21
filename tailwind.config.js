250ccfc36d0c32d7bc90d3fc041b1a015f218457
SiulDev <siuldesing@gmail.com.com>
2025-07-20 21:03:46 -0400

    Spotify clone, first layout

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./components/**/*.{astro,html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
